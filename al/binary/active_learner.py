ac1474b66b335df246bf171efc64adf37bf4178e
rdorado <ruben.dorados@gmail.com>
2016-09-20 08:45:17 -0400

    New experiment

--- al/binary/active_learner.py
+++ al/binary/active_learner.py
@@ -7,6 +7,19 @@ twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle
 
 
 count_vect = CountVectorizer(binary='true')
-X_train_counts = count_vect.fit_transform(twenty_train.data)
-print X_train_counts
+csr = count_vect.fit_transform(twenty_train.data)
+
+start = 0
+counts = [{} for i in range(len(categories))]
+for i, end in enumerate(csr.indptr[1:]):
+    print i
+    for j, val in zip(csr.indices[start:end], csr.data[start:end]):
+      try:
+        counts[twenty_train.target[i]][j] = counts[twenty_train.target[i]][j]+1
+      except KeyError:
+        counts[twenty_train.target[i]][j] = 1  
+        #arr[i,j] = val
+    start = end
+
+#print type(X_train_counts)
 #twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
